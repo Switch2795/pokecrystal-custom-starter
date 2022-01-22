45ecfdf485730b3adee3e2096b662e958e0e26aa
Switch2795 <98136540+Switch2795@users.noreply.github.com>
2022-01-22 15:56:00 -0500

    Update text.asm

--- home/text.asm
+++ home/text.asm
@@ -508,9 +508,16 @@ _ContText::
 	ld a, [wLinkMode]
 	or a
 	call z, UnloadBlinkingCursor
-	; fallthrough
+	jr _ContTextNoPause.not_instant
 
 _ContTextNoPause::
+	ld a, [wOptions]
+	and TEXT_DELAY_MASK
+	cp TEXT_DELAY_FAST
+	jr nz, .not_instant
+	ld c, 15
+	call DelayFrames
+.not_instant
 	push de
 	call TextScroll
 	call TextScroll
@@ -519,6 +526,13 @@ _ContTextNoPause::
 	jp NextChar
 
 ContText::
+	ld a, [wOptions]
+	and TEXT_DELAY_MASK
+	cp TEXT_DELAY_FAST
+	jr nz, .not_instant
+	ld c, 15
+	call DelayFrames
+.not_instant
 	push de
 	ld de, .cont
 	ld b, h
